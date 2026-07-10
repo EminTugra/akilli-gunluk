e8dd1e1f74a9159edd657e48322005779b5e1aa9
Emin Tuğra Çağıl <emintugra99@gmail.com>
2026-07-10 19:03:21 +0300

    Add files via upload

---
+++ akilli-gunluk-sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "akilli-gunluk-v8";
+const APP_SHELL = [
+  "./akilli-gunluk-m3-expressive.html",
+  "./akilli-gunluk.webmanifest",
+  "./akilli-gunluk-icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./akilli-gunluk-m3-expressive.html")))
+  );
+});
